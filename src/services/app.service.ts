b260422d0c1ee2fb4b1e9bcdcbcb216a7f8d3a03
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-08-08 16:56:24 +0300

    host.docker.internal

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,7 +1,7 @@
 import {Quote} from "../entities/Quote";
 
 function getQuote(url: string): Promise<Quote> {
-    return fetch('http://localhost:8080/quotes/random')
+    return fetch('http://host.docker.internal:8080/quotes/random')
         .then(res => res.json())
         .then(res => {
             return res as Quote
