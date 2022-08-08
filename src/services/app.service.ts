872e4971d958564034f8ea95b21144fe3cd84498
Anton Makarenko <makarenkoanton2013@gmail.com>
2022-08-08 17:44:48 +0300

    host.docker.internal

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,9 +1,9 @@
 import {Quote} from "../entities/Quote";
 
-function getQuote(url: string): Promise<Quote> {
-    return fetch('http://host.docker.internal:8080/quotes/random')
+export async function fetchQuote(): Promise<Quote> {
+    return await fetch('http://host.docker.internal:8080/quotes/random')
         .then(res => res.json())
         .then(res => {
             return res as Quote
-        })
+        });
 }
